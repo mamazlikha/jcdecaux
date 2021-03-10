879454376afe299a136fb8a0b64af2af2e5c63a3
AnasFRANCIS <anas.francis@etu.univ-cotedazur.fr>
2021-03-10 16:55:41 +0100

    TD4.2 questions : 1 & 2

--- webFront/materials/main.js
+++ webFront/materials/main.js
@@ -1,1 +1,12 @@
-document.getElementById("workingTest").innerHTML="It works";
+function retrieveAllContracts(){
+    apiKey = document.getElementById("1").value;
+    url = "https://api.jcdecaux.com/vls/v1/contracts?apiKey="+apiKey;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.setRequestHeader("Accept","application/json");
+    xhr.onload = function () {
+        console.log(this.responseText); // http://example.com/test
+      };
+    xhr.send()
+    //console.log(url);
+}
