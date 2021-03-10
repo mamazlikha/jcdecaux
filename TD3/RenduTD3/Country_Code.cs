c8b4779e8b2eb099bb0f5db77d139b56e03175e1
AnasFRANCIS <anas.francis@etu.univ-cotedazur.fr>
2021-03-10 14:40:47 +0100

    TD3 sauf Q4

---
+++ TD3/RenduTD3/Country_Code.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenduTD3
+{
+    enum Country_Code
+    {
+        FR,
+        ES,
+        BE,
+        IE,
+        SI,
+        LU,
+        SE,
+        NO,
+        AU,
+        JP,
+        LT,
+        EN
+    }
+}
---
+++ TD3/RenduTD3/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenduTD3
+{
+    class Position
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+
+        public override string ToString()
+        {
+            return "lat = " + lat + ", lng = " + lng;
+        }
+
+    }
+}
