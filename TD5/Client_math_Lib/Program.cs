2f6f8af6192056b12fce1b9633ffd53ef77260ce
FRANCISAnas <anas.francis@etu.univ-cotedazur.fr>
2021-03-31 16:03:59 +0200

    uodate

---
+++ TD5/Client_math_Lib/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_math_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                     
+
+            
+
+
+            /*Math calculator = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+            Console.WriteLine("2*2 : " + calculator.MultiplyAsync(2, 2).Result);
+            Console.ReadLine();*/
+        }
+    }
+}
---
+++ TD5/MathsLibrary/IMathsOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathsLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        double Add(double v1, double v2);
+        
+        [OperationContract]
+        double Multiply(double v1, double v2);
+
+        [OperationContract]
+        double Substract(double v1, double v2);
+
+        [OperationContract]
+        double Divide(double v1, double v2);
+
+
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+   
+}
