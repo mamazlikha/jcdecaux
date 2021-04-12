312beca0169bc258b97b11c4f7ca51da17dd6289
FRANCISAnas <anas.francis@etu.univ-cotedazur.fr>
2021-04-12 20:44:22 +0200

    seting up proxy

---
+++ projet/BasicClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BasicClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceProxy.ServiceProxyClient proxy = new ServiceProxy.ServiceProxyClient();
+            Console.WriteLine("Hello\n");
+            Console.WriteLine(proxy.GetContacts());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ projet/MyProxy/ServiceProxyImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyProxy
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class ServiceProxyImpl : IServiceProxy
+    {
+        public string GetContacts()
+        {
+            Console.WriteLine("calling getContacts");
+            string uri = "https://api.jcdecaux.com/vls/v1/stations?contract=valence&apiKey=c9c6ff3f25cb517f16421e2c4d191fe0a70ebbfe";
+            ProxyCache<JCDecauxObj> cache = new ProxyCache<JCDecauxObj>();
+            Console.WriteLine(cache.Get(uri));
+            return cache.Get(uri).getjsonObj();
+
+        }
+    }
+}
